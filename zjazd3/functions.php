6add62cc4fe5524499eadb27be86cf33b7e246a4
Michal Jach <92219378+s26234@users.noreply.github.com>
2023-04-27 22:50:07 +0200

    Create functions.php

---
+++ zjazd3/functions.php
@@ -0,0 +1,14 @@
+<?php
+    function dodawanie($number1, $number2){
+      return $number1 + $number2;
+    }
+    function odejmowanie($number1, $number2){
+      return $number1 - $number2;
+    }
+    function mnozenie($number1, $number2){
+      return $number1 * $number2;
+    }
+    function dzielenie($number1, $number2){
+      return $number1 / $number2;
+    }
+    ?>
