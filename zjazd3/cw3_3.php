144567d56b5151211a11e2c4db2d46126b2288d4
Michal Jach <92219378+s26234@users.noreply.github.com>
2023-04-28 07:49:51 +0200

    Correct name to cw3_3.php

--- zjazd3/cw3_3.php
+++ zjazd3/cw3_3.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cw3_2</title>
+    <title>cw3_3</title>
 </head>
 <body>
 <form method="post">
